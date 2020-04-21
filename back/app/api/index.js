637f545e189332eef1f78e324f39716942a9149f
CharlyDucrocq <charly.ducrocq@etu.univ-cotedazur.fr>
2020-04-21 10:57:40 +0200

    back user

--- back/app/api/index.js
+++ back/app/api/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express')
 const QuizzesRouter = require('./quizzes')
 const QuestionRouter = require('./quizzes/questions')
-const AnswerRouter = require('./quizzes/questions/Answers')
+const AnswerRouter = require('./quizzes/questions/answers')
+const UserRouter = require('./users')
 
 const router = new Router()
 router.get('/status', (req, res) => res.status(200).json('ok'))
 router.use('/quizzes', QuizzesRouter)
 router.use('/questions', QuestionRouter)
 router.use('/answers', AnswerRouter)
+router.use('/users', UserRouter)
 
 module.exports = router
